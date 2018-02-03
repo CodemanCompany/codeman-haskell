768b6b8f4d24c0aea5a67d7bfaf8d321b3a53ae9
Gustavo Martínez <tavo962@gmail.com>
2018-02-02 22:48:57 -0600

    Initial commit.

---
+++ functions.hs
@@ -0,0 +1,11 @@
+-- Consecutive
+succ 9
+
+-- Consecutive
+succ 'a'
+
+-- Minimum and maximum
+min 3 5
+max 2 3
+
+max 43 ( succ 3 )
---
+++ operators.hs
@@ -0,0 +1,21 @@
+-- OR, AND
+True || False
+True && False
+
+-- Mod
+10 `mod` 3
+
+-- Different
+True /= False
+
+-- Equal
+True == True
+
+-- Not
+not True
+
+-- Add
+8 + 3.2
+
+-- concatenate 
+"hola" ++ " mundo"
